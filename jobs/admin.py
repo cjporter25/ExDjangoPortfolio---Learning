443bc0cac04391520453a1a5eaa3aaaddbbdb944
cjporter25 <chris.j.porter25@gmail.com>
2023-09-05 20:14:16 -0500

    Finished Admin setup - 09/05/2023

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+# ".models" tells python to import directly from the "models.py" module inside
+#       the same directory
+# We are importing "Job" as that is the name of the model class made in models.py
+from .models import Job
+
 # Register your models here.
+admin.site.register(Job)
--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Job
 
 # Create your views here.
 def chris(request): 
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects
+    return render(request, 'jobs/home.html', {'jobs': jobs})
